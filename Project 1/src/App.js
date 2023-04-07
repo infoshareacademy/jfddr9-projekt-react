8495600b997cc59ed3ceefd5c968a15d20edc202
Robert Dąbrowski <122578857+frontendprogramista@users.noreply.github.com>
2023-04-07 10:54:56 +0200

    initial commit

--- Project 1/src/App.js
+++ Project 1/src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      Put solution here
-    </div>
-  );
+  return <div className="app">Put solution here</div>;
 }
 
 export default App;
