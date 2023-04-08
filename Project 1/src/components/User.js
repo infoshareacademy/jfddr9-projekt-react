9c1e7638237bef21ba82b0ed8cbd5f3ad16a8c77
Bartosz Zagozdon <bart.zagozdon@gmail.com>
2023-04-08 02:05:44 +0200

    Task 5 done

--- Project 1/src/components/User.js
+++ Project 1/src/components/User.js
@@ -1,11 +1,30 @@
 import { Users } from "./Users";
+import { useState } from "react";
 
 export const User = () => {
     console.log(Users);
+
+    const [list, delUser] = useState(Users);
+
+    const handleDelUser = (userName) => {
+
+        const newList = list.filter((user) => {
+            return userName !== user.name;
+        });
+
+        delUser(newList);
+
+    }
+
+    if(list.length===0)
+    {
+        return <span>Users list empty!</span>
+    }
+
     return (
         <ol>
-            {Users.map((user) => {
-                return <li key={user.name}>{user.name} {user.email}</li>
+            {list.map((user) => {
+                return <li key={user.name}>{user.name} {user.email}<button onClick={() => handleDelUser(user.name)}>Usuń</button></li>
             })}
         </ol>
     )
