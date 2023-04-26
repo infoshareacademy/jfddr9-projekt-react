9a8f29dded8c688c3d81ae09a963604cb01cb95b
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-04-26 14:43:16 +0200

    add list from firebase

--- Project_2/src/App.tsx
+++ Project_2/src/App.tsx
@@ -6,24 +6,36 @@ import { AppWrapper } from "./components/AppWrapper";
 import { useEffect, useState } from "react";
 import { filterUserByQuery } from "./utils/filterUserByQuery";
 import { ErrorAlert } from "./components/ErrorAlert";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAzmVCG3zVoQZbJGk57GR2X6tHF9gvqp70",
+  authDomain: "mati-10d63.firebaseapp.com",
+  projectId: "mati-10d63",
+  storageBucket: "mati-10d63.appspot.com",
+  messagingSenderId: "695105336254",
+  appId: "1:695105336254:web:6cbccac834b9b0265a3fef",
+  measurementId: "G-5E6T7JTB8V"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
 function App() {
   const [users, setUsers] = useState<any>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
-
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => {
-        setUsers(data as any);
-        setIsLoading(false);
-      })
-      .catch((e) => {
-        setError(true);
+    const usersRef = collection(db, "List");
+    getDocs(usersRef).then((querySnapshot) => {
+      const users = querySnapshot.docs.map((queryDocumentSnapshot) => {
+        return queryDocumentSnapshot.data();
       });
+      setUsers(users);
+      setIsLoading(false);
+    });
   }, []);
-
   const [query, setQuery] = useState("");
   return (
     <AppWrapper>
@@ -37,5 +49,4 @@ function App() {
     </AppWrapper>
   );
 }
-
 export default App;
